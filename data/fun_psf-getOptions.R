62024d2ae4b8b76226cc19162b81d0f8d772f170
Sean <metamaden@users.noreply.github.com>
2016-10-20 12:11:41 -0700

    psf.getOptions function
    function to coordinate workflow interactively in pyroseqFinder

---
+++ data/fun_psf-getOptions.R
@@ -0,0 +1,16 @@
+function(){
+  x <- readline(message("Do you want to include annotations with your query? (enter y,n or default)"))
+  if(x=="y"){
+    message(paste0("Select what HM450 Manifest annotations to include. Column options are:"))
+    for(i in 1:length(colnames(anno))){message(paste0(i,". ",colnames(anno)[i]))}
+    anno.cols <- readline(message("Enter column numbers to include (separated by ;): "))
+    anno.cols <- as.numeric(gsub("[^0-9]","",unique(unlist(strsplit(anno.cols,";")))))
+    psf.getFlankseq(anno.cols=anno.cols)
+  } else{
+    if(x=="default"){
+      psf.getFlankseq(anno.cols=c(1,2,3,7,8,18,19,20,24,26))
+    } else{
+      psf.getFlankseq()
+    }
+  }
+}
