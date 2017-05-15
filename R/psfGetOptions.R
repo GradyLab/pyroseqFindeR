72028bb5d3618a20245d13299928b1190ce91e13
smaden <metamaden@users.noreply.github.com>
2017-05-15 14:04:22 -0700

    Update psfGetOptions.R

--- R/psfGetOptions.R
+++ R/psfGetOptions.R
@@ -1,11 +1,20 @@
 psfGetOptions <- function(){
   # dependancies
-  require('BSgenome.Hsapiens.UCSC.hg19')
-
+  suppressMessages(require('BSgenome.Hsapiens.UCSC.hg19'))
+  suppressMessages(require("IlluminaHumanMethylation450kprobe"))
+  
   x <- readline(message("Do you want to include annotations with your query? (enter y,n or default)"))
-  if(x=="y"){
+  if(x=="y")
+  {
+    data(IlluminaHumanMethylation450kprobe)
+    anno <- IlluminaHumanMethylation450kprobe
+
     message(paste0("Select what HM450 Manifest annotations to include. Column options are:"))
-    for(i in 1:length(colnames(anno))){message(paste0(i,". ",colnames(anno)[i]))}
+    for(i in 1:length(colnames(anno)))
+    {
+      message(paste0(i,". ",colnames(anno)[i]))
+    }
+    
     anno.cols <- readline(message("Enter column numbers to include (separated by ;): "))
     anno.cols <- as.numeric(gsub("[^0-9]","",unique(unlist(strsplit(anno.cols,";")))))
     psfGetFlankseq(anno.cols=anno.cols)
